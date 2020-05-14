baf5fda938e48f7a9ff8423deb04f15cb8ba278f
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-14 22:54:51 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+
+  @@count = 0
+  @@artists = []
+  @@genres =[]
+
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1
+  end 
+
+  def self.count 
+    @@count 
+  end 
+
+  def self.artists 
+    @@artists 
+  end 
+
+  def self.genres 
+    @@genres 
+  end 
+
+  def self.genre_count
+    @@genres.count 
+  end 
+
+  def self.artist_count
+    @@artists.count 
+  end 
+
+end
+
+  
