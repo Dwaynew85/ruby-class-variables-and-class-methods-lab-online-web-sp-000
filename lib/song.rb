b9de1ee076cfd183818a910f1788f924fef69d95
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-14 22:55:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << @artist 
-    @@genres << @genre 
+    @@artists << @artist
+    @@genres << @genre
   end
 
   def self.count
@@ -35,5 +35,3 @@ class Song
   end
 
 end
-
-  
